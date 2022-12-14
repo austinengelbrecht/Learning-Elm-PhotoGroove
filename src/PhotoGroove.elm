2226248b89d672e00de1a0663dcf72bc64b2ec5e
Austin <aengelbrecht>
2022-12-14 11:58:58 -0600

    chapter 2

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,7 +1,9 @@
 module PhotoGroove exposing (main)
 
+import Browser
 import Html exposing (div, h1, img, text)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 
 urlPrefix = 
@@ -26,6 +28,7 @@ viewThumbnail selectedUrl thumb =
     img 
       [ src (urlPrefix ++ thumb.url) 
       , classList [ ( "selected", selectedUrl == thumb.url ) ]
+      , onClick { description = "ClickedPhoto", data = thumb.url }
       ] 
       []
   
@@ -42,5 +45,18 @@ initialModel =
   }
 
 
+
+update msg model =
+  if msg.description == "ClickedPhoto" then
+    { model | selectedUrl = msg.data }
+
+  else
+    model
+
+
 main = 
-  view initialModel
+  Browser.sandbox
+    { init = initialModel
+    , view = view
+    , update = update
+    }
