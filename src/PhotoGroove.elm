e64fd234d5a714d3072afd8841e4db27e7a231c1
Austin <aengelbrecht>
2022-12-14 13:38:11 -0600

    start of 3.2

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -4,15 +4,23 @@ import Browser
 import Html exposing (div, h1, img, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Array exposing (Array)
 
 
+urlPrefix : String
 urlPrefix = 
   "http://elm-in-action.com/"
 
+type alias Msg =
+  { description : String, data: String }
 
+view : Model -> Html Msg
 view model =
   div [ class "content" ]
     [ h1 [] [text "Photo Grove"] 
+    , button 
+      [ onClick { description = "ClickedSurpriseMe", data = "" } ]
+      [ text "Surprise Me!"]
     , div [ id "thumbnails" ] 
       (List.map (viewThumbnail model.selectedUrl) model.photos
       )
@@ -24,6 +32,7 @@ view model =
     ]
 
 
+viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumb =
     img 
       [ src (urlPrefix ++ thumb.url) 
@@ -33,6 +42,15 @@ viewThumbnail selectedUrl thumb =
       []
   
 
+type alias Photo =
+  { url : String }
+
+type alias Model =
+  { photos: List Photo
+  , selectedUrl : String 
+  }
+
+initialModel : Model
 initialModel = 
   { photos =
     [ { url = "1.jpeg" } 
@@ -41,6 +59,11 @@ initialModel =
     ]
     , selectedUrl = "1.jpeg"
   }
+
+
+photoArray: Array Photo
+photoArray =
+  Array.fromList initialModel.photos
 
 
 update msg model =
