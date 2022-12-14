e28244f19792a713af3cd3e886da494f1a0fc236
Austin <aengelbrecht>
2022-12-14 11:49:18 -0600

    2.2.1

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -3,35 +3,35 @@ module PhotoGroove exposing (main)
 import Html exposing (div, h1, img, text)
 import Html.Attributes exposing (..)
 
--- view model = 
---   div [ class "content" ]
---     [ h1 [] [text "Photo Groove"] 
---     , div [ id "thumbnails"]
---       [ img [src "http://elm-in-action.com/1.jpeg" ] []
---       , img [src "http://elm-in-action.com/2.jpeg" ] []
---       , img [src "http://elm-in-action.com/3.jpeg" ] []
---       ]
---     ]
 
--- variable for photo url
 urlPrefix = 
   "http://elm-in-action.com/"
 
 
--- creates the view for html
 view model =
   div [ class "content" ]
     [ h1 [] [text "Photo Grove"] 
-    , div [ id "thumbnails" ] (List.map viewThumbnail model)
+    , div [ id "thumbnails" ] 
+      (List.map (viewThumbnail model.selectedUrl) model.photos
+      )
+    , img 
+        [ class "large"
+        , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+        ]
+        []
     ]
 
 
--- This function puts the urls together
-viewThumbnail thumb =
-  img [ src (urlPrefix ++ thumb.url) ] []
+viewThumbnail selectedUrl thumb =
+    img 
+      [ src (urlPrefix ++ thumb.url) 
+      , classList [ ( "selected", selectedUrl == thumb.url ) ]
+      ] 
+      []
+  
 
 
--- This is a list of the different photos
+
 initialModel = 
   { photos =
     [ { url = "1.jpeg" } 
