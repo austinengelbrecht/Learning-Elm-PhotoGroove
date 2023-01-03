d6920d0bedae62488d804b82b35894d4f510014c
Austin <aengelbrecht>
2023-01-03 14:55:57 -0600

    Ch 5.2 | Preparing data to be sent to JavaScript

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,4 +1,4 @@
-module PhotoGroove exposing (main)
+port module PhotoGroove exposing (..)
 
 import Browser
 import Html exposing (..)
@@ -106,6 +106,14 @@ type ThumbnailSize
   | Medium
   | Large
 
+
+port setFilters : FilterOptions -> Cmd msg
+
+type alias FilterOptions = 
+  { url : String
+  , filters: List { name : String, amount : Int }
+  }
+
 type alias Photo =
   { url : String 
   , size : Int 
@@ -150,10 +158,10 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     GotRandomPhoto photo ->
-      ( { model | status = selectUrl photo.url model.status }, Cmd.none )
+      applyFilters { model | status = selectUrl photo.url model.status }
 
     ClickedPhoto url ->
-      ({ model | status = selectUrl url model.status }, Cmd.none )
+      applyFilters { model | status = selectUrl url model.status }
 
     ClickedSize size ->
       ({ model | chosenSize = size }, Cmd.none)
@@ -193,6 +201,30 @@ update msg model =
 
     GotPhotos (Err _) ->
       ( { model | status = Errored "Server error!" }, Cmd.none )
+
+
+applyFilters : Model -> ( Model, Cmd Msg )
+applyFilters model = 
+  case model.status of 
+    Loaded photos selectedUrl ->
+      let
+        filters = 
+          [ { name = "Hue", amount = model.hue }
+          , { name = "Ripple", amount = model.ripple }
+          , { name = "Noise", amount = model.noise } 
+          ]
+
+        url = 
+          urlPrefix ++ "large/" ++ selectedUrl
+
+      in
+        ( model, setFilters { url = url, filters = filters } )
+
+    Loading ->
+      ( model, Cmd.none )
+
+    Errored errorMessage ->
+      ( model, Cmd.none )  
 
 
 selectUrl : String -> Status -> Status
