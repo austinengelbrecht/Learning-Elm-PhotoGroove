78bb94f715f18c7e0fab2547485b8dd415db62bd
Austin <aengelbrecht>
2023-01-25 15:38:20 -0600

    SPA init

---
+++ src/Main.elm
@@ -0,0 +1,42 @@
+module Main exposing (..)
+
+import Browser exposing (Document)
+import Html exposing (Html, a, footer, h1, li, nav, text, ul)
+import Hmtl.Attributes exposing (classList, href)
+
+
+type alias Model =
+  {
+  }
+
+
+view : Model -> Document Msg
+view model =
+    { title = "Photo Groove, SPA Sytle" 
+    , body = [ text "This isn't even my final form" ]
+    }
+
+
+type Msg
+  = NothingYet 
+
+
+update : Msg -> Model ->  ( Model, Cmd Msg )
+update msg model =
+  ( Model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+main : Program () Model Msg
+main =
+  Browser.document
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
